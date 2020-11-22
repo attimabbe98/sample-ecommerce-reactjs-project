fb41c723f43ea5d8a1df647908d555fe83f99cf7
attimabbe98 <hosamaniattimabbe96@gmail.com>
2020-11-22 17:52:36 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    <p>welcome to my app</p>
+  <p><Home /></p>
+  <p>{alert("here your page")}</p>
+  </div>
   );
 }
 
---
+++ src/Home.js
@@ -0,0 +1,6 @@
+import react from 'react';
+const Home = ()=>
+{
+    return "Hello World";
+}
+export default Home;
